f6448b396a191c70c0e12080e172154e0b6bacbe
yaomingyang <mingyang@eastmoney.com>
2020-12-29 10:22:23 +0800

    解决网关添加服务发现后启动异常问题

--- src/main/java/com/emily/cloud/gateway/GatewayBootstrap.java
+++ src/main/java/com/emily/cloud/gateway/GatewayBootstrap.java
@@ -2,11 +2,13 @@ package com.emily.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayBootstrap {
 
     public static void main(String[] args) {
