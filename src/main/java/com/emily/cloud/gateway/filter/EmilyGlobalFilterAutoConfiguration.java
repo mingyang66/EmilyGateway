81e9b3382704bcb9a1afd0fdb89edbb42492b7b6
yaomingyang <mingyang@eastmoney.com>
2020-12-29 20:02:21 +0800

    网关新增跨域，超时及过滤器配置

--- src/main/java/com/emily/cloud/gateway/filter/EmilyGlobalFilterAutoConfiguration.java
+++ src/main/java/com/emily/cloud/gateway/filter/EmilyGlobalFilterAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.emily.cloud.gateway.filter;
 
+import org.springframework.cloud.gateway.filter.ReactiveLoadBalancerClientFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +16,7 @@ public class EmilyGlobalFilterAutoConfiguration {
     public EmilyGlobalFilter emilyGlobalFilter() {
         EmilyGlobalFilter emilyGlobalFilter = new EmilyGlobalFilter();
         //设置优先级顺序在{@link org.springframework.cloud.gateway.filter.ReactiveLoadBalancerClientFilter}(10150)过滤器之后，方便获取到真实的请求地址
-        emilyGlobalFilter.setOrder(10151);
+        emilyGlobalFilter.setOrder(ReactiveLoadBalancerClientFilter.LOAD_BALANCER_CLIENT_FILTER_ORDER+1);
         return emilyGlobalFilter;
     }
 }
