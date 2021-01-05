d7de057684aee87f752f6f725a823c99bd795a5c
yaomingyang <mingyang@eastmoney.com>
2021-01-05 14:36:04 +0800

    网关过滤器及断言调优

--- src/main/java/com/emily/cloud/gateway/api/TestController.java
+++ src/main/java/com/emily/cloud/gateway/api/TestController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @program: spring-cloud-gateway
  * @description:
@@ -17,5 +19,11 @@ public class TestController {
     @GetMapping("quickly")
     public String gateway(){
         return "Gateway is quickly";
+    }
+
+    private static AtomicInteger position = new AtomicInteger(0);;
+    public static void main(String[] args) {
+        System.out.println(position.incrementAndGet());
+        System.out.println(position.incrementAndGet());
     }
 }
--- src/main/java/com/emily/cloud/gateway/filter/EmilyGlobalFilterAutoConfiguration.java
+++ src/main/java/com/emily/cloud/gateway/filter/EmilyGlobalFilterAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.emily.cloud.gateway.filter;
 
+import com.emily.cloud.gateway.filter.ratelimit.IpAddressKeyResolver;
 import org.springframework.cloud.gateway.filter.ReactiveLoadBalancerClientFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +12,22 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration(proxyBeanMethods = false)
 public class EmilyGlobalFilterAutoConfiguration {
-
+    /**
+     * 注册日志拦截全局过滤器
+     */
     @Bean
     public EmilyGlobalFilter emilyGlobalFilter() {
         EmilyGlobalFilter emilyGlobalFilter = new EmilyGlobalFilter();
         //设置优先级顺序在{@link org.springframework.cloud.gateway.filter.ReactiveLoadBalancerClientFilter}(10150)过滤器之后，方便获取到真实的请求地址
-        emilyGlobalFilter.setOrder(ReactiveLoadBalancerClientFilter.LOAD_BALANCER_CLIENT_FILTER_ORDER+1);
+        emilyGlobalFilter.setOrder(ReactiveLoadBalancerClientFilter.LOAD_BALANCER_CLIENT_FILTER_ORDER + 1);
         return emilyGlobalFilter;
+    }
+
+    /**
+     * 限流key
+     */
+    @Bean(IpAddressKeyResolver.BEAN_NAME)
+    public IpAddressKeyResolver ipAddressKeyResolver() {
+        return new IpAddressKeyResolver();
     }
 }
