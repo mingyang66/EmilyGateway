5995a10674872ffbf874af50a85216bd479c7292
yaomingyang <mingyang@eastmoney.com>
2021-01-07 11:29:38 +0800

    异常处理

---
+++ src/main/java/com/emily/cloud/gateway/exception/EmilyErrorWebExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.emily.cloud.gateway.exception;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.DefaultErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.reactive.function.server.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @program: EmilyGateway
+ * @description: 自定义异常处理
+ * @author: 姚明洋
+ * @create: 2021/01/06
+ */
+public class EmilyErrorWebExceptionHandler extends DefaultErrorWebExceptionHandler {
+
+    private static Logger logger = LoggerFactory.getLogger(EmilyErrorWebExceptionHandler.class);
+
+    public EmilyErrorWebExceptionHandler(ErrorAttributes errorAttributes, WebProperties.Resources resources, ErrorProperties errorProperties, ApplicationContext applicationContext) {
+        super(errorAttributes, resources, errorProperties, applicationContext);
+    }
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    @Override
+    protected Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(request, options);
+        Map<String, Object> errorData = new LinkedHashMap<>();
+        errorData.put("status", errorAttributes.get("status"));
+        errorData.put("message", StringUtils.join(errorAttributes.get("error")));
+        logger.error(errorAttributes.toString());
+        return errorData;
+    }
+}
